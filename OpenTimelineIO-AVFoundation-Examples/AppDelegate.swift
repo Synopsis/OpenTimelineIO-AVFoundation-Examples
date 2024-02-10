83e6baedd78cf556181c2c1082fde76b37fe978b
vade <vade@vade.info>
2024-02-10 18:45:59 -0500

    Initial Commit

---
+++ OpenTimelineIO-AVFoundation-Examples/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  OpenTimelineIO-AVFoundation-Examples
+//
+//  Created by Anton Marini on 2/10/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ OpenTimelineIO-AVFoundation-ExamplesTests/OpenTimelineIO_AVFoundation_ExamplesTests.swift
@@ -0,0 +1,36 @@
+//
+//  OpenTimelineIO_AVFoundation_ExamplesTests.swift
+//  OpenTimelineIO-AVFoundation-ExamplesTests
+//
+//  Created by Anton Marini on 2/10/24.
+//
+
+import XCTest
+@testable import OpenTimelineIO_AVFoundation_Examples
+
+final class OpenTimelineIO_AVFoundation_ExamplesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
